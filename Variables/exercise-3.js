a5d865796035b346e828a498d96526d6f42764b1
Ben Clarkson <ben.a.clarkson@gmail.com>
2022-01-29 14:09:02 -0500

    Clean up question organization in Variables chapter and update misspelling

--- Variables/exercise-3.js
+++ Variables/exercise-3.js
@@ -2,13 +2,13 @@
  * # Exercise 3 #
 
  * Task: What happens when you run the following program? Why do we get that result?
- * 
+ 
  * {
  *   let foo = 'bar';
  * }
  * 
  * console.log(foo);
- * 
+
  * Solution 1:
  * The code will result in an error because the variable `foo` is declared within a block, but the log function attempts to call it outside of said block.
 **/
